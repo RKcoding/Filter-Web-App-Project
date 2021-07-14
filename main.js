1727ea43f591709067a7530886b3da00449fc36e
RKcoding <73450077+RKcoding@users.noreply.github.com>
2021-07-13 21:24:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 function preload() {
-    
+
 }
 
 function setup() {
-    canvas = createCanvas(640, 480);
-    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 480);
+    video.center();
+    poseNet = ml5.poseNet(video, ModelLoaded);
+    poseNet.on("pose", GotPoses);
+}
+
+function ModelLoaded() {
+    console.log("poseNet is initialized");
+}
+
+function GotPoses(results) {
+    console.log(results);
+    console.log("Nose x = " + results[0].pose.nose.x);
+    console.log("Nose y = " + results[0].pose.nose.y);
 }
 
 function draw() {
-    
+
 }
 
 function OnClick() {
